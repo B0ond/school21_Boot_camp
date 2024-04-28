00682e6acb2cac7007b2dced182f905036158ebd
B0ond <amiram19@yandex.ru>
2024-04-29 00:47:10 +0300

    Second commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int number = getInt();
+        if (number < 2) {
+            System.out.println("Illegal Argument");
+            System.exit(-1);
+        }
+        int iterations = 0;
+        boolean isPrimeNumber = isPrime(number, iterations);
+        System.out.println("   " + isPrimeNumber + " " + iterations);
+
+    }
+
+    public static int getInt(){
+        System.out.println("Enter number: ");
+        return sc.nextInt();
+    }
+
+    public static boolean isPrime(int number, int iterations){
+        if (number <= 1){
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        if (number % 2 == 0){
+            return false;
+        }
+        for(int i = 3; i <= Math.sqrt(number) + 1; i+=2){;
+            iterations++;
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//1 задание
+//public static int {
+//    int number =  479598;
+//    int sum = 0;
+//    while (number > 0) {
+//        sum = sum + number % 10;
+//        number = number / 10;
+//    }
+//    return sum;
+//}
