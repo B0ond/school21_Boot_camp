447b8e8075c39f47dabf9f285328dbf950d158ac
Amir <amiram19@yandex.ru>
2024-05-02 17:01:10 +0300

    222

--- java/src/day00_2.java
+++ java/src/day00_2.java
@@ -2,26 +2,36 @@ import java.util.Scanner;
 
 public class day00_2 {
     private static final Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
-        int enter_number = getInt();
+    private static int count_prime = 0;
 
+    public static void main(String[] args) {
+        getInt();
+        System.out.println("Count of coffee-request - " + count_prime);
     }
 
-        public static int getInt(){    //прием цифр
-            System.out.println("Enter number: ");
-            return sc.nextInt();
+    public static void getInt(){   //Прием даных
+        while(true){
+            int number = sc.nextInt();
+            if (number == 42) {
+                System.out.println("Count of coffee-request - " + count_prime);
+                System.exit(0);
+            }
+            int sum = digitSum(number);
+            boolean prime_or_not = isPrime(sum);
+            if (prime_or_not) {
+                count_prime++;
+            }
         }
+    }
 
     public static int digitSum(int n) {   //суммирует цифры в числе
-        int number =  479598;
         int sum = 0;
-        while (number > 0) {
-            sum = sum + number % 10;
-            number = number / 10;
+        while (n > 0) {
+            sum = sum + n % 10;
+            n = n / 10;
         }
         return sum;
     }
-
 
     public static boolean isPrime(int number){   //проверка суммы на простоту
         if (number <= 1){
@@ -40,9 +50,25 @@ public class day00_2 {
         }
         return true;
     }
-
-    public static int countPrime(int number){   //суммирует простые числа
-        int count = 0;
-        return isPrime(number) ? ++count : count;
-    }
 }
+
+
+
+
+//public static int getInt(){   //Прием даных
+//    while(true){
+//        System.out.println("Enter number: ");
+//        int number = sc.nextInt();
+//        if (number == 42) {
+//            int result = countPrime(number);
+//            System.out.println("Result of countPrime: " + result);
+//            System.out.println("Program stopped.");
+//            System.exit(0);
+//        }
+//        int sum = digitSum(number);
+//        boolean prime_or_not = isPrime(sum);
+//        if (prime_or_not) {
+//            count_prime++;
+//        }
+//    }
+//}
